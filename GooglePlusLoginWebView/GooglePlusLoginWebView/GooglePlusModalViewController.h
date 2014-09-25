2bb56999b2ae9464cfc5280465c8598fb8cec64c
Davide Tarasconi <info@davidetarasconi.net>
2014-09-25 09:37:14 +0200

    Added a modal view controller

---
+++ GooglePlusLoginWebView/GooglePlusLoginWebView/GooglePlusModalViewController.h
@@ -0,0 +1,13 @@
+//
+//  GooglePlusModalViewController.h
+//  GooglePlusLoginWebView
+//
+//  Created by Davide Tarasconi on 25/09/14.
+//  Copyright (c) 2014 Davide Trasconi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GooglePlusModalViewController : UIViewController
+
+@end
