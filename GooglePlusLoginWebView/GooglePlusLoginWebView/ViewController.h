faf8d66448db543d793e0eea8eb87b60d33ef367
Davide Tarasconi <info@davidetarasconi.net>
2014-09-25 09:05:03 +0200

    Third commit
    OK, I got the project name right this time

---
+++ GooglePlusLoginWebView/GooglePlusLoginWebView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GooglePlusLoginWebView
+//
+//  Created by Davide Tarasconi on 25/09/14.
+//  Copyright (c) 2014 Davide Trasconi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
